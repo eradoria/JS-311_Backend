47abb285eeba1c8996be85fb6916595e302ce589
eradoria <eradoria@gmail.com>
2022-09-10 16:23:50 -0500

    cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const bodyParser = require("body-parser");
 const promoRouter = require("./routes/routes");
 const authRouter = require("./routes/authRoutes");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 4000;
