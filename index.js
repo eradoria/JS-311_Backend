91232257983a4775929d9520c3314044186df6af
eradoria <eradoria@gmail.com>
2022-09-18 19:28:19 -0500

    added CORS line to app.get

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const promoRouter = require("./routes/routes");
 const authRouter = require("./routes/authRoutes");
 const cors = require("cors");
 
-
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/promo", promoRouter);
 app.use("/auth", authRouter);
-app.use(cors())
-
 
 app.get("/", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
   res.send("Welcome to our server!");
 });
-
-
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
